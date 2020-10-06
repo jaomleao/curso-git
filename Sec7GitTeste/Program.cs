b59f923d51d6c521f483c72fb28e4ceafe4d7509
Joao Leao <jaomleao@gmail.com>
2020-10-06 10:03:47 -0300

    Adicionado nova mensagem de bom dia

--- Sec7GitTeste/Program.cs
+++ Sec7GitTeste/Program.cs
@@ -4,6 +4,8 @@ namespace Sec7GitTeste {
 	class Program {
 		static void Main(string[] args) {
 			Console.WriteLine("Hello World!");
+			Console.WriteLine("Mensagem teste - alterado");
+			Console.WriteLine("Bom dia!");
 		}
 	}
 }
